e51ce911cb4dcee336394d1ef57684dcae4c0e11
Cai Shengjian <1294797029@qq.com>
2020-06-09 16:36:08 +0800

    a little api function

--- app/src/main/java/com/example/myapplication/ForecastRepo.kt
+++ app/src/main/java/com/example/myapplication/ForecastRepo.kt
@@ -1,6 +1,12 @@
 package com.example.myapplication
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.example.myapplication.api.CurrentWeather
+import com.example.myapplication.api.createOpenWeatherMapService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import kotlin.random.Random
 
 class ForecastRepo {
@@ -11,5 +17,25 @@ class ForecastRepo {
         val randomValues=List(7){ Random.nextFloat().rem(100)*100 }
         val forecastItems=randomValues.map { DailyForeCast(temp=it,desc="a good day") }
         _weeklyForecast.value=forecastItems
+    }
+
+    fun loadCurrentForecastByNetWork(zipCode: String){
+        val call= createOpenWeatherMapService().currentWeather(zipCode,"imperial","appid")
+        call.enqueue(object : Callback<CurrentWeather>{
+            override fun onFailure(call: Call<CurrentWeather>, t: Throwable) {
+                Log.e(ForecastRepo::class.java.simpleName,"err loading current weather",t)
+            }
+
+            override fun onResponse(
+                call: Call<CurrentWeather>,
+                response: Response<CurrentWeather>
+            ) {
+                val weather=response.body()
+                if(weather!=null){
+                    Log.e(ForecastRepo::class.java.simpleName,"data loaded need to be used")
+                }
+            }
+
+        })
     }
 }
